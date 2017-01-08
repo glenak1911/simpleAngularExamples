56c591229e7544f45514210670bc6caa97b666d6
Glen Knight <glenak1911@gmail.com>
2017-01-07 19:33:57 -0500

    Added routing and add'l views

--- js/controllers/simpleController.js
+++ js/controllers/simpleController.js
@@ -1,8 +1,26 @@
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp',['ngRoute']);
 
 //Create Controllers object literal
 var controllers = {};
 
+//Routes Configuration
+myApp.config(function ($routeProvider){
+  $routeProvider
+    .when('/',
+      {
+        controller:'SimpleController',
+        templateUrl: 'views/View1.html'
+      })
+    .when('/view2',
+      {
+          controller:'SimpleController',
+          templateUrl:'views/View1.html'
+      })
+      .otherwise({redirectTo:'/'});
+});
+
+//adding SimpleController to the object literal
+//as an anonoymous function key pair
 controllers.SimpleController = function($scope){
   $scope.customers=[
     {name:'Dave Jones',city:'Phoenix'},
@@ -10,6 +28,13 @@ controllers.SimpleController = function($scope){
     {name:'Heedy Wahlin',city:'Chandler'},
     {name:'Thomas Winter',city:'Seattle'}
   ]
+
+  $scope.addCustomer = function(){
+    $scope.customers.push(
+      {name:$scope.newCustomer.name,
+      city:$scope.newCustomer.city
+    });
+  }
 }
 
 myApp.controller(controllers);
